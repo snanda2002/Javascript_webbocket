8d4f98e089f78cb11ca933c4d9d6f48d3288c34a
snanda2002 <152247525+snanda2002@users.noreply.github.com>
2023-12-11 05:06:45 +0000

    file added

---
+++ 02-Intermediate/ArrowFunction.js
@@ -0,0 +1,11 @@
+/*
+Arrow Function:---
+    Arrow function also called as fat arrow function is a features in es6
+    in javascript to write consize function in js
+*/
+
+ function timesTwo(params){return params * 2}
+ console.log(timesTwo(4));
+
+var timesTwo=params=>params * 2
+console.log(timesTwo(4));
